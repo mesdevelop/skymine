4943a7d225adfe15aecddd73b756efb37eb85b3d
mesdevelop <155332540+mesdevelop@users.noreply.github.com>
2024-08-11 05:40:09 +0200

    Uploading new code

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 1040;
 
 // Endpoint di controllo della salute
 app.get('/healthz', (req, res) => {
